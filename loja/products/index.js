13a935eeacc162b8bc0a6e5fb587a9276110e1e9
AndersonAugusto <anderson.ferrari@hausz.com.br>
2022-07-07 23:48:30 -0300

    feature/checkout

--- loja/products/index.js
+++ loja/products/index.js
@@ -1,10 +1,12 @@
 
 
-let carrinho = []
+let carrinhoOld = JSON.parse(localStorage.getItem('carrinhoLojinha')) || []
+
+let carrinho = [...carrinhoOld]
 
 let carrinhoCount = document.getElementById('carrinhoCount');
 
-carrinhoCount.innerText = 0
+carrinhoCount.innerText = carrinho.length
 
 renderListOfProducts()
 function renderListOfProducts(){
@@ -17,8 +19,11 @@ function renderListOfProducts(){
                 <img class="imgProduct" src="${product.imagem}" alt="">
                 <p class="title">${product.name}</p>
                 <p class="description">${product.description} </p>
-                <button class="carrinho" onclick="addCarrinho(this)" data-type="${product.name}" 
-                name="${product.preco}" value="${product.imagem}">
+                <button class="carrinho" onclick="addCarrinho(this)"
+                    data-type="${product.name}" 
+                    data-desc="${product.description}"
+                    name="${product.preco}" 
+                    value="${product.imagem}">
                     <img src="../../imagens/carrinho-de-compras.png" alt="">
                     <span>R$ ${product.preco} </span>
                 </button>
@@ -28,24 +33,75 @@ function renderListOfProducts(){
     });
 }
 
-
 function addCarrinho(element){
-    carrinhoCount.innerText = carrinho.length
-    
+    checkout.innerHTML = ""
+
     let cardSelected = {
         imagem: element.value,
         name: element.dataset.type,
-        preco: element.name
+        preco: element.name,
+        description: element.dataset.desc
     }
     carrinho.push(cardSelected)
+    carrinhoCount.innerText = carrinho.length
 
-    console.log('1')
     localStorage.setItem('carrinhoLojinha' , JSON.stringify(carrinho))
+    renderCheckout()
 }
 
+const checkout = document.getElementById('checkout');
+const fundoPreto = document.getElementById('fundoPreto');
+const modal = document.getElementById('modal')
+const valorTotal = document.getElementById('valorTotal')
+
+renderCheckout()
+function renderCheckout(){
+
+    if(carrinho.length < 1){
+        checkout.innerHTML = ' Vazio'
+        valorTotal.innerText = `R$ 0,00`
+    } else {
+
+        let count = 0
+        carrinho.forEach(produtc => {
+            console.log(produtc.preco)
+            count =  count + parseFloat(produtc.preco)
+            
+            let moldeCheckout = `
+            <div class="boxProduct">
+                <img src="${produtc.imagem}" alt="">
+                <div class="descriptionCheckout">   
+                    <h3>${produtc.name} </h3>
+                    <p>${produtc.description} </p>
+                </div>
+                <p class="price">${produtc.preco}</p>
+            </div>
+            `
+            checkout.innerHTML += moldeCheckout
+        });
+        valorTotal.innerText = `R$ ${count.toFixed(2)}` || 0
+    }
+}
+    
+function clearCarrinho(){
+    console.log('1')
+    localStorage.removeItem('carrinhoLojinha')
+    checkout.innerHTML = ""
+}
 function closeModal(){
-    document.getElementById('checkout').style.display = 'none'   
+    checkout.classList.remove('showAnimation')
+    fundoPreto.classList.remove('fundoPreto')
+    checkout.classList.add('closeAnimation')
+   
+    setTimeout(() => {
+        modal.style.display = 'none'
+        // checkout.style.display = 'none'
+    }, 200)
 }
 function showCheckout(){
-    document.getElementById('checkout').style.display = 'block'
+    modal.style.display = 'block'
+    // checkout.style.display = 'block'
+    checkout.classList.remove('closeAnimation')
+    checkout.classList.add('showAnimation')
+    fundoPreto.classList.add('fundoPreto')
 }
