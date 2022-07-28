c1d9cb4964f41d24b27a1e20c0f74ad52bdf3888
AndersonAugusto <anderson.ferrari@hausz.com.br>
2022-07-27 23:01:39 -0300

    feature/animation

---
+++ animation.js
@@ -0,0 +1,14 @@
+
+
+const padrao = {
+    scale: 0.8,
+    opacity: 0,
+    easing:  'ease-out',
+};
+
+const leftOpacity = {};
+const rightOpacity = {};
+
+ScrollReveal().reveal('.anime-right' , padrao);
+ScrollReveal().reveal('.anime-opacity-left' , padrao);
+ScrollReveal().reveal('.anime-opacity-right' , padrao);
