c10af3443ac5a9045716e7eb5797354051957f51
Anderson Augusto Ferrari <andersonaugustoferrari@hotmail.com>
2022-12-03 00:19:25 -0300

    feature/iniciandofoguete

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.scss';
 
-//IMAGENS
-import imagePerfil from './img/ander.png'
-import logo from './img/logoPort.png'
-import instagram from './img/instagram.png'
-import facebook from './img/facebook.png'
-import whatsapp from './img/whatsapp.png'
-import github from './img/github.png'
-import linkedin from './img/linkedin.png'
+//images
+import foguete from './assets/img/foguete.png'
+import fogo from './assets/img/fogo.png'
 
 function App() {
-  
-  return (
-    <main className='container'>
 
-      <section className='first-section'>
-           <div className='container-card'>
-              <div className='info-card'>
-                <img className='personPhoto' src={ imagePerfil } alt="perfil-photo" />
-                  <h1> Anderson Ferrari </h1>
-                  <p>Full stack Developer</p>
-                  <div className='social-icons'>
-                    <img src={ instagram } alt="perfil-photo" />
-                    <img src={ linkedin } alt="perfil-photo" />
-                    <img src={ whatsapp } alt="perfil-photo" />
-                    <img src={ github } alt="perfil-photo" />
-                  </div>
-              </div>
-              <div className='text-card'>
-                <h3> Anderson Augusto Ferrari </h3>
-                <h4> Full Stack Developer </h4>
-                <p>
-                Sou desenvolvedor Full Stack, possuo sólidos conhecimentos sobre Node.js (Express) com arquitetura MVC. Tenho grande experiência com Front-end, sendo: HTML, CSS , Javascript , bootstrap e jquery. Também tenho conhecimento em desenvolvimento de SPA com os Frameworks: Angular e React.js. Obtive experiência com plataformas de gerenciamento de projetos e sistema de versionamento, sendo: Github, Trello e Asana.
-                </p>
-              </div>
+  const [finishedTimeout, setFinishedTimeout] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFinishedTimeout(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <section className='container-top'>
+        <div className='caixa-foguete'>
+          <div className='foguete'>
+            <img
+              className="carcaca"
+              src={foguete} />
+            {finishedTimeout &&
+              <img
+                className="fogo"
+                src={fogo} />
+            }
           </div>
+        </div>
       </section>
-      <section className='secont-section'>
-          <div>
-              2
-          </div>
+      <section className='container'>
+        sdasd
       </section>
-    </main>
+    </>
   );
 }
 
